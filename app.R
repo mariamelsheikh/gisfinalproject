dceb0accba9ff637fc4397266d7b292a02d3eedd
Mariam E <elsheikhmz@gmail.com>
2020-04-25 00:48:16 -0400

    building the app

--- app.R
+++ app.R
@@ -1,7 +1,158 @@
+#load libraries
+
 library(shiny)
+library(leaflet)
 library(dplyr)
 library(shinydashboard)
 library(tidyr)
+library(sf)
+
+## load data
+
+
+nbh_to <- read_sf("data/nbh.shp") %>% 
+    st_transform(crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+
+accessibility <- read_sf("data/ratio.shp") %>% 
+    st_transform(crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+
+sociodemo <- read_sf("data/sociodemo.shp") %>% 
+    st_transform(crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+
+names(sociodemo) <- c("Shape Area","Type","Dwellings","Households","GeoUID","Population",
+                      "Adjusted Population (previous Census)","PR_UID","CMA_UID","CSD_UID",
+                      "CD_UID","province_code","cms_code","census_tract","population",
+                      "population_densitykm2","average_age",
+                      "average household size","knowledge_eng",
+                      "knowledge_french","kowledge_both","knowledge_neither",
+                      "first_english","first_french",
+                      "first_both","first_neither","median income of households in 2015 ($)",
+                      "median after-tax income of households in 2015",
+                      "number household in 2015",
+                      "under 5000","5000 to 9999","10000 to 14999","15000 to 19999",
+                      "20000 to 24999","25000 to 29999","30000 to 34999","35000 to 39999",
+                      "40000 to 44999","45000 to 49999","50000 to 59999","60000 to 69999",
+                      "60000 to 79999","80000 to 89999","90000 to 99999","100000 and over",
+                      "average after-tax income of households in 2015","low income LIM-AT", 
+                      "prevalence of low income LIM-AT","low income LICO-AT",
+                      "prevalence LICO-AT",
+                      "knowledge of languages for the population in private households",
+                      "knowledge_official language_private households",
+                      "knowledge_eng_private households",
+                      "knowledge_french_private households", 
+                      "knowledge_non-official language_private households",
+                      "Total - Citizenship for the population in private households",
+                      "canadian citizens","Not Canadian citizens", 
+                      "Total - Immigrant status for the population in private households",
+                      "non-immigrants", "immigrants", "Non-permanent residents",
+                      "Immigrants_2011 to 2016", 
+                      "Immigrants_2006 to 2010", "Immigrants_2001 to 2005",
+                      "Immigrants_2001 to 2010", 
+                      "Immigrants_1991 to 2000", "Immigrants_1981 to 1990",
+                      "Immigrants_Before 1981", 
+                      "Total - Generation status for the population in private households", 
+                      "First generation", "Second generation", "Third generation or more", 
+                      "Admission category for the immigrant population in private households", 
+                      "Economic immigrants", "Immigrants sponsored by family", "Refugees", 
+                      "other immigrants", 
+                      "Total - Aboriginal identity for the population in private households", 
+                      "Non-Aboriginal identity", "Aboriginal identity", 
+                      "Total - Visible minority for the population in private households", 
+                      "Total visible minority population","Not a visible minority",
+                      "Total - Private households by tenure",
+                      "Total - diploma or degree for the population aged 15 years and over in private households", "No certificate, diploma or degree", "Secondary",
+                      "Postsecondary", "Employment rate",
+                      "Unemployment rate",
+                      "Total - Population aged 15 years and over by Labour force status", 
+                      "In the labour force", "Not in the labour force","geometry")
+
+sociodemo <- sociodemo %>% 
+    select("Shape Area","Type","Dwellings","Households","GeoUID","Population",
+           "PR_UID","CMA_UID","CSD_UID",
+           "CD_UID","province_code","cms_code","census_tract","population",
+           "population_densitykm2","average_age", "100000 and over",
+           "average after-tax income of households in 2015", 
+           "prevalence of low income LIM-AT",
+           "Total - Immigrant status for the population in private households",
+           "immigrants","Total - Visible minority for the population in private households",
+           "Total visible minority population",
+           "Total - diploma or degree for the population aged 15 years and over in private households",
+           "No certificate, diploma or degree","Postsecondary","Unemployment rate","Employment rate")
+
+sociodemo <- sociodemo %>% 
+    rename("total_immigrants_vm" = "Total - Immigrant status for the population in private households",
+           "visibleminority" = "Total visible minority population",
+           "total_edu" = "Total - diploma or degree for the population aged 15 years and over in private households",
+           "nodegree" = "No certificate, diploma or degree",
+           "avg_aftertax_income_hh" = "average after-tax income of households in 2015",
+           "prevlence_low_income_limat" = "prevalence of low income LIM-AT") %>% 
+    select(-"Total - Visible minority for the population in private households")
+
+sociodemo <- sociodemo %>% 
+    mutate(per_immigrants = (immigrants/total_immigrants_vm)*100,
+           per_visibleminority = (visibleminority/total_immigrants_vm)*100,
+           per_postsecondary = (Postsecondary/total_edu)*100,
+           per_nodegree = (nodegree/total_edu)*100)
+
+healthcenters <- read_sf("data/healthcenters.shp") %>% 
+    st_transform(crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+longlat <- as.data.frame(st_coordinates(healthcenters))
+
+
+healthcenters <- healthcenters %>% mutate(long = longlat$X,
+                                                  lat = longlat$Y)
+
+
+# base map
+basemap <- leaflet() %>%
+    # Base groups
+    addTiles() %>% 
+    addPolygons(data = nbh_to,
+                weight = 3,
+                color = "grey",
+                highlight = highlightOptions(weight = 5,
+                                     color = "black",
+                                     fillOpacity = 0.1,
+                                     bringToFront = TRUE),
+                label = sprintf(
+                    "<strong>%s</strong>",
+                    nbh_to$AREA_NA
+                ) %>% lapply(htmltools::HTML),
+                labelOptions = 
+                    labelOptions(style = list("font-weight" = "normal", 
+                                              padding = "3px 8px"),
+                                 textsize = "15px",
+                                 direction = "auto")) %>%
+    addCircleMarkers(data = healthcenters[healthcenters$type == "Hospital",],
+                     group = "Hospital",
+                     color = "green",
+                     stroke = T, radius = 5) %>%
+    addCircleMarkers(data = healthcenters[healthcenters$type == "Community Health Center",],
+                     group = "Community Health Center",
+                     color = "turquoise",
+                     stroke = T,radius = 5) %>%
+    addCircleMarkers(data = healthcenters[healthcenters$type == "Walk-in Clinic",],
+                     group = "Walk-in Clinic",
+                     color = "blue",
+                     stroke = T,radius = 5) %>%
+    addCircleMarkers(data = healthcenters[healthcenters$type == "Family Medical Center",],
+                     group = "Family Medical Center",
+                     color = "lightblue", 
+                     stroke = T,radius = 5) %>%
+    addCircleMarkers(data = healthcenters[healthcenters$type == "Family Medical Teams",],
+                     group = "Family Medical Teams", 
+                     color = "springgreen4",
+                     stroke = T,radius = 5) %>%
+    addCircleMarkers(data = healthcenters[healthcenters$type == "Nurse Practitioner Clinic",],
+                     group = "Nurse Practitioner Clinic",
+                     color = "yellow", 
+                     stroke = T,radius = 5) %>%
+    addLayersControl(overlayGroups = c("Hospital", "Community Health Center","Walk-in Clinic",
+                                       "Family Medical Center","Family Medical Teams",
+                                       "Nurse Practitioner Clinic"), 
+                     options = layersControlOptions(collapsed = F)) %>% 
+    addProviderTiles("CartoDB.DarkMatter")
+
 
 
 #Define UI for data upload app
@@ -11,29 +162,50 @@ ui <- dashboardPage(
     dashboardHeader(title = "Health care services in Toronto",
                     titleWidth = 300),
     
-    #Sidebar contents (tabs)
-    dashboardSidebar(width = 170,
-                     sidebarMenu(
-                         menuItem("Introduction",tabName = "intro",icon=icon("book-open")))),
-    
-    
-    
-    #Main panel for displaying outputs
-    dashboardBody(
-        #organizing what will go in the different tabs
-        tabItems(
-            #First tab "intro" and connecting it to the readME file in the directory
-            tabItem(tabName = "intro"),
-            #Second tab "code" and connecting it to this code file
-            tabItem(tabName = "code")
-            #third tab content "HIV" and organizing the different sections that will go in
-            
-        ))
+    dashboardSidebar(disable = T),
+   
+     dashboardBody(
+         tags$head(tags$style(HTML('.skin-blue .main-header .logo {
+          background-color: #3c8dbc;}
+        .skin-blue .main-header .logo:hover {background-color: #3c8dbc;
+        }
+      '))),
+         fluidRow(
+         box(selectizeInput("variable", "Choose Two Variables",
+                         choices = c("population density",
+                                     "healthcare accessibility ratio",
+                                     "immigrants",
+                                     "visible minority",
+                                     "average income after-tax",
+                                     "households with income >=100k",
+                                     "prevalence of low income households",
+                                     "postsecondary degree","no degree",
+                                     "employment rate","unemployment rate"),
+                         multiple = T, options = list(maxItems = 2)),
+             width = 4,solidHeader = TRUE, height = 110)),
+         fluidRow(
+             box(leafletOutput(outputId = "firstmap"), width = 12 ,height = 430)
+         ),
+         fluidRow(
+             box(leafletOutput(outputId = "secondmap"), width = 12 ,height = 430)
+         )
+     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-}
+
+    output$firstmap <- renderLeaflet({
+        
+     basemap
+        
+    })
+    output$secondmap <- renderLeaflet({
+        
+        basemap
+    })
+    
+    }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
